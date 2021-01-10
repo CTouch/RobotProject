f094eda8ab353e576987594a48edd63705c40c2d
xuebashuoge <1172508020@qq.com>
2021-01-10 13:39:50 +0800

    normal commit

--- Check.cpp
+++ Check.cpp
@@ -15,12 +15,12 @@ bool Check_Safe()
 		if (abs(feedback[i].Load) > MAX_LOAD)
 		{
 			ret = true;
-			std::cout << "Load Unsafe!\n";
+			std::cout << "Load Unsafe! Joint" << i << " Load " << feedback[i].Load << std::endl;
 		}
 		if (abs(feedback[i].Speed) > MAX_VEL)
 		{
 			ret = true;
-			std::cout << "Speed Unsafe!\n";
+			std::cout << "Speed Unsafe! Joint" << i << " Speed " << feedback[i].Speed << std::endl;
 		}																			
 		if (ret) break;
 	}
